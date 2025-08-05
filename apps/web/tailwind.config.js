c52accfec81368b652c86df7dac0d7a77ac23148
Nestor Mamani <nmamanipantoja@gmail.com>
2025-08-04 19:51:34 -0500

    feat: Update primary color in Tailwind config to a specific hex value

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -20,7 +20,8 @@ module.exports = {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
+          //DEFAULT: 'hsl(var(--primary))',
+          DEFAULT: '#00FE03',
           foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
